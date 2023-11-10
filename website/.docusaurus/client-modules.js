05d255ba95203f272ba81dfc33045ce090aff00c
Osama Jandali <osamajandaly@gmail.com>
2023-11-10 17:45:07 +0200

    Use GSF doc template

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/joe/Code/if-docs/website/node_modules/infima/dist/css/default/default.css'),
-  require('/home/joe/Code/if-docs/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/joe/Code/if-docs/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/joe/Code/if-docs/website/src/css/custom.css'),
-];
